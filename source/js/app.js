c6ae38916bbd87dacb538eaa20b380c8a62ce670
Rafael Birkmann <rafkafu@yahoo.de>
2015-01-17 09:32:08 +0100

    Added movies

--- source/js/app.js
+++ source/js/app.js
@@ -1,15 +1,15 @@
-angular.module('myApp',[]).
-config(function($routeProvider){
-	$routeProvider.
-	when('/',{templateUrl:'templates/directory.html'}).
-	when('/movie/:id',{templateUrl:'/templates/view.html',controller:'viewController'}).
-	otherwise({redirectTo:'/'})
-}).
-controller('directoryController',['$scope','$http',function($scope,$http){
+angular.module('myApp',[])
+.config(function($routeProvider){
+	$routeProvider
+		.when('/',{templateUrl:'templates/directory.html'})
+		.when('/movie/:id',{templateUrl:'/templates/view.html',controller:'viewController'})
+		.otherwise({redirectTo:'/'})
+})
+.controller('directoryController',['$scope','$http',function($scope,$http){
 	$http.get('/data/data.json').success(function(data){
 		$scope.movies=data
 	})
-}]).
-controller('viewController',['$scope','$routeParams',function($scope,$routeParams){
+}])
+.controller('viewController',['$scope','$routeParams',function($scope,$routeParams){
 	$scope.movie = $scope.movies[$routeParams.id]
 }])
