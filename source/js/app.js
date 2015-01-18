7b9827befd19061d55c64c63cf529a7077fab0ad
Rafael Birkmann <rafkafu@yahoo.de>
2015-01-18 03:01:32 +0100

    Added clock

--- source/js/app.js
+++ source/js/app.js
@@ -1,15 +1,43 @@
-angular.module('myApp',[])
-.config(function($routeProvider){
-	$routeProvider
-		.when('/',{templateUrl:'templates/directory.html'})
-		.when('/movie/:id',{templateUrl:'/templates/view.html',controller:'viewController'})
-		.otherwise({redirectTo:'/'})
-})
-.controller('directoryController',['$scope','$http',function($scope,$http){
+"use strict";
+angular.module('myApp', ['ngRoute']);
+
+angular.module('myApp').config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl:'templates/directory.html',
+            controller: 'MainCtrl'
+        })
+        .when('/movie/:id', {
+            templateUrl:'/templates/view.html',
+            controller: 'viewController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+angular.module('myApp').controller('MainCtrl', function () {
+    //console.log('MainCtrl invoked!');
+});
+
+angular.module('myApp').controller('directoryController',['$scope','$http',function($scope,$http){
 	$http.get('./data/data.json').success(function(data){
 		$scope.movies=data
 	})
-}])
-.controller('viewController',['$scope','$routeParams',function($scope,$routeParams){
+}]);
+
+angular.module('myApp').controller('viewController',['$scope','$routeParams',function($scope,$routeParams){
 	$scope.movie = $scope.movies[$routeParams.id]
-}])
+}]);
+
+angular.module('myApp').controller('TimeCtrl', function ($scope, $timeout) {
+   $scope.tickInterval = 1000 //ms
+
+    var tick = function () {
+        $scope.clock = Date.now() // get the current time
+        $timeout(tick, $scope.tickInterval); // reset the timer
+    }
+
+    // Start the timer
+    $timeout(tick, $scope.tickInterval);
+});
